42d01384538aab3d4c342e06e23e39b1d57e7fe0
rkddnr330 <rkddnr330@gmail.com>
2022-04-28 23:32:10 +0900

    Initial Commit

---
+++ NC1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NC1
+//
+//  Created by Park Kangwook on 2022/04/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NC1/NC1App.swift
@@ -0,0 +1,17 @@
+//
+//  NC1App.swift
+//  NC1
+//
+//  Created by Park Kangwook on 2022/04/28.
+//
+
+import SwiftUI
+
+@main
+struct NC1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
